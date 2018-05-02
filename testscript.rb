b4d6d81a23963abac098493a92c73f9558d3892e
mikehidalgo <mike.hidalgo@outlook.com>
2018-05-02 09:14:38 -0700

    updated homepage

--- testscript.rb
+++ testscript.rb
@@ -1,7 +1,7 @@
 
 class Testscript < Formula
-  desc ""
-  homepage ""
+  desc "initial draft"
+  homepage "https://github.com/MikeHidalgo/homebrew-testtap.git"
   url "https://github.com/MikeHidalgo/testscript/archive/v1.0.0.tar.gz"
   sha256 "b9c21de0669ebbf1a14a4e6b62aed4c4787207d248d8fcbcc991384b84f46597"
 
