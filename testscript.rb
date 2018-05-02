bf37c71ca55337302689c090027467f290825343
Mike <mike.hidalgo@outlook.com>
2018-05-01 23:45:04 -0700

    uupdate

--- testscript.rb
+++ testscript.rb
@@ -3,10 +3,12 @@ class Testscript < Formula
   desc ""
   homepage ""
   url "https://github.com/MikeHidalgo/testscript/archive/v1.0.0.tar.gz"
+  version "1.0.0"
   sha256 "b9c21de0669ebbf1a14a4e6b62aed4c4787207d248d8fcbcc991384b84f46597"
 
+  
   def install
         bin.install "testscript"
   end
-
+  
 end
